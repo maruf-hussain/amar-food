17965d9010c0d19e00dc41310af39d27dfb53b85
maruf-hussain <engmarufhussain@gmail.com>
2023-10-06 14:28:51 -0700

    fjaf

--- src/Pages/Home/Shared/Header/Header.jsx
+++ src/Pages/Home/Shared/Header/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
             <li><a>Item 3</a></li>
     </>
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar fixed z-20 opacity-40 bg-black text-white max-w-7xl">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
