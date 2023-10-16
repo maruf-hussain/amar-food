784646f59de5d9851fe5a3ef7035c29e9b5d246e
maruf-hussain <engmarufhussain@gmail.com>
2023-10-16 22:31:02 +0600

    falfj

--- src/Pages/Home/Shared/Header/Header.jsx
+++ src/Pages/Home/Shared/Header/Header.jsx
@@ -2,20 +2,21 @@
 
 const Header = () => {
     const navItems = <>
-     <li><a>Item 1</a></li>
+     <li><a>Home</a></li>
+     <li><a>Contact Us</a></li>
             <li tabIndex={0}>
               <details>
-                <summary>Parent</summary>
+                <summary>Our Menu</summary>
                 <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
+                  <li><a>Co</a></li>
                   <li><a>Submenu 2</a></li>
                 </ul>
               </details>
             </li>
-            <li><a>Item 3</a></li>
+            <li><a>Dash Borad</a></li>
     </>
     return (
-        <div className="navbar fixed z-20 opacity-40 bg-black text-white max-w-7xl">
+        <div className="navbar fixed z-20  text-white max-w-7xl">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
